ff0796be3db8a85a2a519ade31ab6fc01f67b582
Brobit <alannmaricourt15@gmail.com>
2025-04-07 14:43:45 +0200

    [architecture] le code est mis sous design atomique

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { Animated, Text, StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import BottomTabNavigator from './components/organisms/navbar.tsx';
+import SplashScreen from './pages/loadingScreen.tsx';
+import Colors from './themes/colors.tsx';
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  const [showMainContent, setShowMainContent] = useState(false); // State to control content display
+  const fadeAnim = useRef(new Animated.Value(1)).current; // Opacity starting at 1
+  const translateYAnim = useRef(new Animated.Value(0)).current; // Starting position
+
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'tomato', // useless?
+      background: Colors.backgroundLight,
+    },
+  };
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'BoldFont': require('../assets/fonts/Poppins-Bold.ttf'),
+        'RegularFont': require('../assets/fonts/Poppins-Regular.ttf'),
+      });
+      setFontLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  const handleAnimationEnd = () => {
+    setShowMainContent(true);
+  };
+
+  if (!fontLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Pilou</Text>
-      <StatusBar style="auto" />
+        <SplashScreen
+          fadeAnim={fadeAnim}
+          translateYAnim={translateYAnim}
+          onAnimationEnd={handleAnimationEnd}
+        />
+        <NavigationContainer theme={MyTheme}>
+          <BottomTabNavigator />
+        </NavigationContainer>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFDF6',
   },
 });
---
+++ src/pages/searchScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const SearchScreen = ({navigation}: {navigation: any}) => {
+  return (
+    <View>
+      <Button title="Go to Settings" onPress={() => navigation.navigate('Settings')} />
+    </View>
+  );
+};
+
+export default SearchScreen;
