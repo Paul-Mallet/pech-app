819f608b7428627d4d2dd701add48880a0da120c
Brobit <alannmaricourt15@gmail.com>
2025-04-23 15:11:36 +0200

    41 - fin selection resolved

--- src/components/organisms/questions.tsx
+++ src/components/organisms/questions.tsx
@@ -6,6 +6,7 @@ import QuestionExpandable from "./questionExpandable.tsx";
 import QuestionModel from "../../models/questions.model.tsx";
 import { getFishByAnswer } from "../../services/fish.service.tsx";
 import { useFishList } from "../../@config/fishListContext.tsx";
+import ResearchAnswerModel from "../../models/researchAnswer.model.tsx";
 
 type QuestionsProps = {
     navigation : any,
@@ -47,35 +48,37 @@ const Questions : React.FC<QuestionsProps> = ({navigation, questionsParams}) => 
 
     type AnswerField = "bodyType" | "fin" | "eye"
 
-    const handleQuestionPress = (type: AnswerField, id : number) => {
-      switch (type) {
-        case "bodyType":
-            setAnswers({
-              ...answers,
-              bodyType : id
-            });
-          break;
-        case "fin":
-            setAnswers((prev : any) => ({
-              ...prev,
-              fin : prev.fin.includes(id) 
-                ? prev.fin.filter((oneId : number) => oneId != id)
-                : [...prev.fin, id]
-            }))
-          break;
-        case "eye":
-            setAnswers({
-              ...answers,
-              eye : id
-            });
-          break;
-        default:
-          break;
+    const handleQuestionPress = (field: AnswerField, id: number) => {
+      if (field !== "fin") {
+        setAnswers({
+          ...answers,
+          [field]: id,
+        });
+        return;
       }
-        // setQuestionType(type);
-        // setModalVisible(true);
-    };
+    
+      const finGroup = questionsParams.finsIds.find(group =>
+        group.ids.includes(id)
+      );
+    
+      if (!finGroup) return;
 
+      setAnswers((prev : any) => {
+        const currentFinIds = prev.fin || [];
+
+        const isSelected = currentFinIds.includes(id);
+
+        const filtered = currentFinIds.filter((id : number) => !finGroup.ids.includes(id));
+
+        const updated = isSelected ? filtered : [...filtered, id];
+    
+        return {
+          ...prev,
+          fin: updated,
+        };
+      });
+    };
+    
     useEffect(() => {
       const updateFish = async () => {
         const updatedFishList = await getFishByAnswer(answers);
