77f060693505da4794267ccdd5ee53ef66d54b96
Brobit <alannmaricourt15@gmail.com>
2025-04-23 12:10:57 +0200

    41 - api implemetation done
    api implemetation done, but not test, need to change the quesiton component

--- src/components/organisms/questions.tsx
+++ src/components/organisms/questions.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View , Text, ScrollView, FlatList} from "react-native";
 import QuestionStyles from "../../styles/organisms/questionStyles.tsx";
 import { useAnswers } from "../../@config/answerContext.tsx";
 import QuestionExpandable from "./questionExpandable.tsx";
 import QuestionModel from "../../models/questions.model.tsx";
+import { getFishByAnswer } from "../../services/fish.service.tsx";
+import { useFishList } from "../../@config/fishListContext.tsx";
 
 type QuestionsProps = {
     navigation : any,
@@ -38,14 +40,50 @@ const questions = [
 
 const Questions : React.FC<QuestionsProps> = ({navigation, questionsParams}) => {
     const {answers, setAnswers } = useAnswers();
+    const {setFishList} = useFishList();
     const styles = QuestionStyles();
     // const [visibleModal, setModalVisible] = useState(false);
     // const [questionType, setQuestionType] = useState<string | null>(null);
 
-    const handleQuestionPress = (type: string) => {
+    type AnswerField = "bodyType" | "fin" | "eye"
+
+    const handleQuestionPress = (type: AnswerField, id : number) => {
+      switch (type) {
+        case "bodyType":
+            setAnswers({
+              ...answers,
+              bodyType : id
+            });
+          break;
+        case "fin":
+            setAnswers((prev : any) => ({
+              ...prev,
+              fin : prev.fin.includes(id) 
+                ? prev.fin.filter((oneId : number) => oneId != id)
+                : [...prev.fin, id]
+            }))
+          break;
+        case "eye":
+            setAnswers({
+              ...answers,
+              eye : id
+            });
+          break;
+        default:
+          break;
+      }
         // setQuestionType(type);
         // setModalVisible(true);
     };
+
+    useEffect(() => {
+      const updateFish = async () => {
+        const updatedFishList = await getFishByAnswer(answers);
+        setFishList(updatedFishList);
+      }
+
+      updateFish();
+    }, [answers])
 
     return (
         <View style={[styles.mainDiv, { width: '100%', flex: 1 }]}>
@@ -66,7 +104,6 @@ const Questions : React.FC<QuestionsProps> = ({navigation, questionsParams}) => 
             />
         </View>
     )
-
 }
 
 export default Questions
